dd58bce047a5a9fac8d1367d715663934874eede
ivanfslee <ivanfslee@gmail.com>
2019-11-29 15:13:08 -0800

    Added comment component and semantic-ui css integration

--- src/index.js
+++ src/index.js
@@ -1,20 +1,58 @@
-// Import React and ReactDOM libraries 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
-// Create a react component
 const App = () => {
     return (
-        <div>
-            <label class="label" for="name">Enter name:</label>
-            <input id="name" type="text" />
-            <button style={{ backgroundColor: 'blue', color: 'white' }}>Submit</button>
+        <div className="ui container comments">
+            <div className="comment">
+
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()} />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
+            <div className="comment">
+
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()} />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
+            <div className="comment">
+
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()} />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
         </div>
     );
 };
 
-// Take the react components and render it to screen
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
+
