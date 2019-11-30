9254678bd5e7719c115b92b54b83798a4f62dea5
ivanfslee <ivanfslee@gmail.com>
2019-11-30 12:46:28 -0800

    Added new ApprovalCard component

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import faker from 'faker';
 
 
 const CommentDetail = props => {
     return (
         <div className="comment">
-
             <a href="/" className="avatar">
                 <img alt="avatar" src={props.avatar} />
             </a>
@@ -16,7 +14,9 @@ const CommentDetail = props => {
                 <div className="metadata">
                     <span className="date">{props.timeAgo}</span>
                 </div>
-                <div className="text">{props.comment}</div>
+                <div className="text">
+                    {props.comment}
+                </div>
             </div>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,15 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
-
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
-    const avatar = faker.image.avatar();
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" timeAgo="Yesterday at 5:00PM" comment="Wooooo!!" avatar={avatar} />
-            <CommentDetail author="Coco" timeAgo="Today at 2:34PM" comment="Great post~" avatar={avatar} />
-            <CommentDetail author="Stark" timeAgo="Today at 3:45PM" comment="Nice!" avatar={avatar} />
+            <ApprovalCard />
+            <CommentDetail 
+                author="Sam" 
+                timeAgo="Yesterday at 5:00PM" 
+                comment="Wooooo!!" 
+                avatar={faker.image.avatar()} 
+            />
+            <CommentDetail 
+                author="Coco" 
+                timeAgo="Today at 2:34PM" 
+                comment="Great post~" 
+                avatar={faker.image.avatar()} 
+            />
+            <CommentDetail 
+                author="Stark" 
+                timeAgo="Today at 3:45PM" 
+                comment="Nice!" 
+                avatar={faker.image.avatar()} 
+            />
         </div>
     );
 };
