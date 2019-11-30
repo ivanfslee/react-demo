c83eee2543f9acf4cd7eb22f6eb1c3f2f5c72381
ivanfslee <ivanfslee@gmail.com>
2019-11-30 13:04:02 -0800

    Nested components inside of components

--- src/index.js
+++ src/index.js
@@ -7,25 +7,48 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
     return (
         <div className="ui container comments">
-            <ApprovalCard />
-            <CommentDetail 
-                author="Sam" 
-                timeAgo="Yesterday at 5:00PM" 
-                comment="Wooooo!!" 
-                avatar={faker.image.avatar()} 
-            />
-            <CommentDetail 
-                author="Coco" 
-                timeAgo="Today at 2:34PM" 
-                comment="Great post~" 
-                avatar={faker.image.avatar()} 
-            />
-            <CommentDetail 
-                author="Stark" 
-                timeAgo="Today at 3:45PM" 
-                comment="Nice!" 
-                avatar={faker.image.avatar()} 
-            />
+            <ApprovalCard>
+                <div>
+                    <h4>Warning</h4>
+                    Are you sure you want to do this?
+                </div>
+                <CommentDetail 
+                    author="Sam" 
+                    timeAgo="Yesterday at 5:00PM" 
+                    comment="Wooooo!!" 
+                    avatar={faker.image.avatar()} 
+                />
+                <CommentDetail 
+                    author="Sam" 
+                    timeAgo="Yesterday at 5:00PM" 
+                    comment="Wooooo!!" 
+                    avatar={faker.image.avatar()} 
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Sam" 
+                    timeAgo="Yesterday at 5:00PM" 
+                    comment="Wooooo!!" 
+                    avatar={faker.image.avatar()} 
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Coco" 
+                    timeAgo="Today at 2:34PM" 
+                    comment="Great post~" 
+                    avatar={faker.image.avatar()} 
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Stark" 
+                    timeAgo="Today at 3:45PM" 
+                    comment="Nice!" 
+                    avatar={faker.image.avatar()} 
+                />
+            </ApprovalCard>
         </div>
     );
 };
